6dc1d624cb2b1a1f6810df6e328f93553d6020a7
Jones <jonesdhy@hotmail.com>
2017-03-31 13:19:40 -0300

    Ajustes Finais

---
+++ src/main/webapp/scripts/functions.js
@@ -0,0 +1,16 @@
+
+function resizeDown(){
+  divPrincipal = document.getElementById("biografia");
+  divPrincipal.style.backgroundColor = 'rgba(0,0,0,1)';
+  divPrincipal.style.margin = '30px 20%';
+  divPrincipal.style.transition = '0.5s';
+}
+
+function resizeUp(){
+  divPrincipal = document.getElementById("biografia");
+  divPrincipal.style.backgroundColor = 'rgba(0,0,0,.5)';
+  divPrincipal.style.margin = '10px 10%';
+  divPrincipal.style.transition = '0.5s';
+}
+
+
